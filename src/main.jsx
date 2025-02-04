5aadffdcf45c2e51141e3e93701cd7f7a4501712
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-04 10:04:39 +0700

    Menambah Dan Mengubah Font

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import './dist/css/main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
