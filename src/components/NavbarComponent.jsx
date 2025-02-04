6a2e4a5dd270363a36aa831e3d5a5d500f4e9f1d
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-04 10:49:07 +0700

    Navbar (2)

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,26 +1,41 @@
+import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { navLinks } from "../data/index";
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const NavbarComponent = () => {
+    const [changeColor, setChangeColor] = useState(false);
+
+    const changeBackgroundColor = () => {
+        if (window.scrollY > 10) {
+            setChangeColor(true);
+        } else {
+            setChangeColor(false);
+        }
+    }
+
+    useEffect(() => {
+        changeBackgroundColor();
+        window.addEventListener("scroll", changeBackgroundColor);
+    });
     return (
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
+            <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Ngoding</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto">
+                        <Nav className="mx-auto text-center">
                             {navLinks.map((Link) => {
                                 return (
-                                    <div key={Link.id}>
-                                        <NavLink to={Link.path}>{Link.text}</NavLink>
+                                    <div className="navlink" key={Link.id}>
+                                        <NavLink to={Link.path} className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""} end >{Link.text}</NavLink>
                                     </div>
                                 );
                             })}
                         </Nav>
-                        <div>
-                            <button>Join With Us</button>
+                        <div className="text-center">
+                            <button className="btn btn-outline-danger rounded-1">Join With Us</button>
                         </div>
                     </Navbar.Collapse>
                 </Container>
