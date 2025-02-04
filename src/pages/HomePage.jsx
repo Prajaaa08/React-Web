6b91db2e1af6af13cbae305fb23ae875c32d427e
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-04 11:19:40 +0700

    Homepage (2)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from "..assets/img/hero.png";
+
 const HomePage = () => {    
     return (
         <div className="homepage">
-            <header className="w-100 min-vh-100"></header>
+            <header className="w-100 min-vh-100">
+                <Container>
+                    <Row className="header-box d-flex align-items-center">
+                        <Col>
+                            <h1>
+                                Temukan <br /> <span>Bakat Kreatifmu</span> Bersama Kami!
+                            </h1>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. VeriLorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis expedita doloribus est porro consequuntur impedit.
+                            </p>
+                            <button>Lihat Kelas</button>
+                            <button>Lihat Promo</button>
+                        </Col>
+                        <Col>
+                            <img src={HeroImage} alt="hero-img" />
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
             <div className="kelas w-100 min-vh-100"></div>
         </div>
     );
