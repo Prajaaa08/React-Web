a4fffd19a5f4ca8e4375f7311de1fba12689a2f2
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-10 12:25:15 +0700

    Section Kelas (3) Dan Swiper

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,81 +1,145 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { kelasTerbaru } from "../data/index";
+import { kelasTerbaru, dataSwiper } from "../data/index";
 import HeroImage from "../assets/img/hero.png";
 
-const HomePage = () => {    
-    const navigate = useNavigate();
-    return (
-        <div className="homepage">
-            <header className="w-100 min-vh-100">
-                <Container>
-                    <Row className="header-box d-flex align-items-center">
-                        <Col lg="6">
-                            <h1 className="mb-4">
-                                Temukan <br /> <span>Bakat Kreatifmu</span> Bersama Kami!
-                            </h1>
-                            <p className="mb-4">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. VeriLorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis expedita doloribus est porro consequuntur impedit.
-                            </p>
-                            <button className="btn btn-danger btn-lg-rounded-1 me-2 mb-xs-0 mb-2">Lihat Kelas</button>
-                            <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">Lihat Promo</button>
-                        </Col>
-                        <Col lg="6" className="pt-lg-0 pt-5">
-                            <img src={HeroImage} alt="hero-img" />
-                        </Col>
-                    </Row>
-                </Container>
-            </header>
-            <div className="kelas w-100 min-vh-100">
-                <Container>
-                    <Row>
-                        <Col>
-                        <h1 className="text-center fw-bold">Kelas Terbaru</h1>
-                        <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        {kelasTerbaru.map((kelas) => {
-                            return <Col key={kelas.id}>
-                                <img src={kelas.image} alt="unsplash.com" className="w-100 mb-5 rounded-top" />
-                                <div className="star mb-2">
-                                    <i className={kelas.star1}></i>
-                                    <i className={kelas.star2}></i>
-                                    <i className={kelas.star3}></i>
-                                    <i className={kelas.star4}></i>
-                                    <i className={kelas.star5}></i>
-                                </div>
-                                <h5 className="mb-5 px-3">{kelas.title}</h5>
-                                <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
-                                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
-                                    <button className="btn btn-danger rounded-1">
-                                        {kelas.buy}
-                                    </button>
-                                </div>
-                            </Col>;
-                        })}
-                    </Row>
-                    <Row>
-                        <Col className="text-center">
-                        <button className="btn btn-success rounded-5 btn-lg" onClick={()=>navigate("/kelas")}>
-                            Lihat Semua Kelas
-                            <i className="fa-solid fa-chevron-right ms-1"></i>
-                        </button>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            <div className="tetimonial py-5">
-                <Container>
-                        <Row>
-                            <Col>
-                                <h1 className="text-center fw-bold ">Testimonial</h1>
-                            </Col>
-                        </Row>
-                </Container>
-            </div>
-        </div>
-    );
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="homepage">
+      <header className="w-100 min-vh-100">
+        <Container>
+          <Row className="header-box d-flex align-items-center pt-lg-5">
+            <Col lg="6">
+              <h1 className="mb-4">
+                Temukan <br /> <span>Bakat Kreatifmu</span> Bersama Kami!
+              </h1>
+              <p className="mb-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                VeriLorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis expedita doloribus est porro consequuntur impedit.
+              </p>
+              <button className="btn btn-danger btn-lg-rounded-1 me-2 mb-xs-0 mb-2">
+                Lihat Kelas
+              </button>
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+                Lihat Promo
+              </button>
+            </Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
+              <img src={HeroImage} alt="hero-img" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Kelas Terbaru</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas) => {
+              return (
+                <Col key={kelas.id}>
+                  <img
+                    src={kelas.image}
+                    alt="unsplash.com"
+                    className="w-100 mb-5 rounded-top"
+                  />
+                  <div className="star mb-2">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button
+                className="btn btn-success rounded-5 btn-lg"
+                onClick={() => navigate("/kelas")}
+              >
+                Lihat Semua Kelas
+                <i className="fa-solid fa-chevron-right ms-1"></i>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="tetimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold ">Testimonial</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 5,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+                {dataSwiper.map((data) => {
+                    return <SwiperSlide key={data.id}>
+                        <p>{data.desc}</p>
+                        <div>
+                            <img src={data.image} alt="" />
+                            <div>
+                                <h5>{data.name}</h5>
+                                <p>{data.skill}</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>;
+                })}
+            </Swiper>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
