796471659675543c4e3fd1cf3ea2dcd604bec279
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-12 12:41:15 +0700

    Section footer (1)

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,5 +1,33 @@
+import {Container, Row, Col} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col>
+            <h3 className="fw-bold">Ngoding</h3>
+            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam similique nam accusamus eligendi perspiciatis minima animi </p>
+            <div className="no">
+              <Link className="text-decoration-none">
+                <i className="fa-brands fa-whatsapp"></i>
+                <p>+62 896-5254-2130</p>
+              </Link>
+            </div>
+            <div className="mail">
+              <Link className="text-decoration-none">
+              <i className="fa-regular fa-envelope"></i>
+              <p>person-email@gmail.com</p>
+              </Link>
+            </div>
+          </Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  )
 };
 
 export default FooterComponent;
