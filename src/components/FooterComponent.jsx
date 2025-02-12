a772133da0830f1be7e25cb7e68c169140ccaab9
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-12 15:11:08 +0700

    Section Footer(3)

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -6,10 +6,13 @@ const FooterComponent = () => {
   return (
     <div className="footer py-5">
       <Container>
-        <Row>
-          <Col>
+        <Row className="d-flex justify-content-between">
+          <Col lg="5">
             <h3 className="fw-bold">Ngoding</h3>
-            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam similique nam accusamus eligendi perspiciatis minima animi </p>
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              similique nam accusamus eligendi perspiciatis minima animi{" "}
+            </p>
             <div className="no mb-1 mt-4">
               <Link className="text-decoration-none">
                 <i className="fa-brands fa-whatsapp"></i>
@@ -18,12 +21,12 @@ const FooterComponent = () => {
             </div>
             <div className="mail">
               <Link className="text-decoration-none">
-              <i className="fa-regular fa-envelope"></i>
-              <p className="m-0">person-email@gmail.com</p>
+                <i className="fa-regular fa-envelope"></i>
+                <p className="m-0">person-email@gmail.com</p>
               </Link>
             </div>
           </Col>
-          <Col className="d-flex flex-column col-lg-2 col">
+          <Col className="d-flex flex-column col-lg-2 col mt-lg-0 mt-5">
             <h5 className="fw-bold">Menu</h5>
             <Link to="">Home</Link>
             <Link to="kelas">Kelas</Link>
@@ -31,13 +34,15 @@ const FooterComponent = () => {
             <Link to="faq">FAQ</Link>
             <Link to="syaratketen">Syarat & Ketentuan</Link>
           </Col>
-          <Col>
+          <Col lg="4" className="mt-lg-0 mt-5">
             <h5>Subscribe untuk info menarik</h5>
             <div className="subscribes">
               <input type="text" placeholder="Subscribe..." />
-            <button>Subscribe</button>
+              <button className="btn btn-danger rounded-end rounded-0">
+                Subscribe
+              </button>
             </div>
-            <div className="social">
+            <div className="social mt-3">
               <i className="fa-brands fa-facebook"></i>
               <i className="fa-brands fa-twitter"></i>
               <i className="fa-brands fa-linkedin"></i>
@@ -45,9 +50,16 @@ const FooterComponent = () => {
             </div>
           </Col>
         </Row>
+        <Row>
+          <Col>
+            <p className="text-center px-md-0 px-3">
+              &copy; Copyright {new Date().getFullYear()} by <span className="fw-bold">Nabil Praja</span> , All Right Reserved
+            </p>
+          </Col>
+        </Row>
       </Container>
     </div>
-  )
+  );
 };
 
 export default FooterComponent;
