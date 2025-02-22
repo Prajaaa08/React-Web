2cf177eec94f595d58dfe9240594fa1afdf96918
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-22 11:36:45 +0700

    Menambahkan Function Scroll To Top

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 import './dist/css/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
