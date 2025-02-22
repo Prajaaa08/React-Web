b6d2aa7125800cc3e874c8f2787b2b8dadd4e33b
Prajaaa08 <lestariaagustina@gmail.com>
2025-02-22 11:27:53 +0700

    Menambahkan Page Syarat & Ketentuan (2)

--- src/pages/SyaratKetentuanPage.jsx
+++ src/pages/SyaratKetentuanPage.jsx
@@ -5,18 +5,127 @@ import "../dist/css/main.css";
 const SyaratKetenPage = () => {
   return (
     <div className="syarat-ketentuan-page">
-      <div className="syarat-ketentuan">
+      <div className="syarat-ketentuan min-vh-100">
         <Container>
           <Row>
             <Col>
               <h1 className="fw-bold text-center mb-2">Syarat & Ketentuan</h1>
-              <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+                ipsa mollitia molestias a earum quae consectetur at et
+                reiciendis exercitationem, veniam maiores similique, natus sunt
+                tempora tenetur! At nisi aspernatur optio et! Exercitationem
+                aspernatur eligendi voluptatem ut quidem porro totam veritatis
+                nulla sint nisi! Excepturi maxime ipsam animi neque voluptatum!
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure
+                saepe vero nesciunt aspernatur ducimus corrupti deleniti
+                doloribus rerum ratione aperiam eius rem, corporis harum et
+                ullam necessitatibus. Aliquid delectus cum laboriosam ipsa neque
+                nostrum aut laborum inventore! Pariatur, error nemo.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
+                excepturi, aut, a vel sed eligendi nobis consequuntur earum
+                ullam velit ipsa! Natus repellat alias esse, explicabo
+                aspernatur accusantium unde minima earum quibusdam autem ipsam
+                tempore expedita ex fuga inventore perspiciatis ipsum. Natus
+                vero vel illum doloribus, deserunt ipsa eveniet non magni neque,
+                ea sit labore velit rem, optio exercitationem! Praesentium.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident voluptates libero, laboriosam, enim saepe
+                reprehenderit voluptatibus quo dicta culpa esse eos aliquam
+                nostrum. Similique alias debitis vitae laborum et, ea ex,
+                voluptatibus, tempore ducimus fugit repudiandae earum fugiat
+                velit saepe consequatur ab voluptates? Inventore itaque quaerat
+                accusamus minima ab cum, nisi dignissimos nostrum, reiciendis,
+                in quasi amet. Dicta totam qui eaque possimus iusto ea est fugit
+                laudantium atque laboriosam eveniet, dolorum maiores consectetur
+                eligendi similique illo asperiores impedit. Hic, quasi.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                assumenda dolorem eveniet, dolor odio maiores temporibus. Sed
+                blanditiis laudantium temporibus magnam qui repellat, tempore
+                commodi fugiat itaque animi non, tempora iure illo autem, nam at
+                aliquid molestiae nihil ratione unde aspernatur quos cupiditate!
+                Labore, sint? Veritatis inventore quia quisquam enim sit, et
+                deserunt architecto rerum rem dolores, accusamus atque
+                voluptatum!
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum,
+                aspernatur quis repellendus architecto sapiente non voluptas
+                voluptatum laboriosam rem quam praesentium, reiciendis eius
+                repudiandae nobis nulla illum. Aut temporibus quia illo
+                cupiditate itaque aperiam dignissimos omnis error corporis,
+                libero assumenda perspiciatis porro quidem quis autem, nulla
+                expedita iure modi sequi, optio consectetur vero. Eaque
+                exercitationem iusto amet voluptatibus modi sit eveniet earum
+                aliquid incidunt illo pariatur, dignissimos, eligendi quod quia
+                obcaecati delectus vero explicabo cumque, necessitatibus sed!
+                Expedita, numquam excepturi!
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Lorem</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Distinctio dolores, ipsam excepturi quis adipisci similique
+                soluta id perferendis alias veritatis. Totam voluptatibus saepe
+                sit rerum. Quisquam dolorem perferendis odit ipsam adipisci!
+                Laboriosam, quod. Laboriosam iure sapiente eum dolorem non ab?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+                saepe ratione quas nisi dolorem, iusto deserunt cum ex dolorum
+                cupiditate incidunt voluptatem alias itaque temporibus officia
+                impedit in! Non quia numquam voluptatum perspiciatis hic ut
+                similique sint labore, ipsum placeat unde repellendus sequi nemo
+                omnis. Hic nam necessitatibus expedita rerum non numquam vero ut
+                facere repudiandae porro earum vitae a, soluta enim tempora
+                aperiam ducimus neque possimus repellat praesentium cum
+                blanditiis! Natus nobis quo esse ipsam saepe ad doloribus
+                inventore?
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloremque repellat voluptas quae dolorem autem numquam at, nam
+                necessitatibus nostrum laudantium non id nihil, enim vitae quo?
+                Impedit vitae quis est provident quae animi atque optio alias.
+                Soluta quas possimus maxime debitis, quam beatae amet officiis,
+                quidem magnam nostrum in quo voluptate repudiandae
+                necessitatibus voluptas suscipit sequi tempore natus? Aut,
+                excepturi.
+              </p>
             </Col>
           </Row>
         </Container>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SyaratKetenPage
+export default SyaratKetenPage;
